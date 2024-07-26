a6861c0416265aea449c865b9b3b328e6d905eef
Tal Segal <tlsgl62@gmail.com>
2024-07-26 15:46:01 +0300

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Bono-App/',
   plugins: [
     checker({ typescript: false }), react()
   ],
